8c16c124cc92544fbe14efd75153dc3b9323f18b
Marin Bareta <mbareta@extensionengine.com>
2016-09-15 11:52:42 +0200

    Add admin to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-   has_one :racer
+  has_one :racer
 end
