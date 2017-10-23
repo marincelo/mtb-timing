32e9106a8613d61623750166969a31a149da6d42
marincelo <mbareta@extensionengine.com>
2017-10-23 09:44:20 +0200

    Strip tag_id before save

--- app/models/start_number.rb
+++ app/models/start_number.rb
@@ -1,3 +1,11 @@
 class StartNumber < ApplicationRecord
   belongs_to :racer, optional: true
+
+  before_save :strip_tag_id
+
+  private
+
+  def strip_tag_id
+    self.tag_id = tag_id.strip
+  end
 end
